776db6914844f72377cced6a9b3a51c63f8b049a
Brian Cristante <none>
2018-05-08 15:08:28 -0400

    Use a template for the index

--- polls/views.py
+++ polls/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request: HttpRequest) -> HttpResponse:
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    body = ', '.join(q.question_text for q in latest_questions)
-    return HttpResponse(body)
+    context = {
+        'latest_questions': Question.objects.order_by('-pub_date')[:5]
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse("You're looking at question %s" % question_id)
