ae691c24e2d1cfb09fa4cb678999570c548005d4
Brian Cristante <none>
2018-05-08 14:48:17 -0400

    Add placeholder views

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're looking at the results of question %s" % question_id)
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s" % question_id)
