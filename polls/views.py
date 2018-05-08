311d79da9d664602c7bc2d954229386a2dc4b60e
Brian Cristante <none>
2018-05-08 14:55:58 -0400

    List recent questions in index

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Question
+
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    body = ', '.join(q.question_text for q in latest_questions)
+    return HttpResponse(body)
 
 def detail(request: HttpRequest, question_id: int) -> HttpResponse:
     return HttpResponse("You're looking at question %s" % question_id)
