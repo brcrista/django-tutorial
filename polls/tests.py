b359e30030e72b3d92f330179f59f90786b97479
Brian Cristante <none>
2018-05-09 18:31:31 -0400

    Add more tests for `is_new`

--- polls/tests.py
+++ polls/tests.py
@@ -6,7 +6,15 @@ from django.utils import timezone
 from .models import Question
 
 class QuestionModelTests(TestCase):
-    def test_reject_future_pub_date(self):
+    def test_old_pub_date_is_not_new(self):
+        q = Question(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertFalse(q.is_new())
+
+    def test_new_pub_date_is_new(self):
+        q = Question(pub_date=timezone.now() - datetime.timedelta(hours=3))
+        self.assertTrue(q.is_new())
+
+    def test_future_pub_date_is_not_new(self):
         future = timezone.now() + datetime.timedelta(days=30)
         q = Question(pub_date=future)
         self.assertFalse(q.is_new())
